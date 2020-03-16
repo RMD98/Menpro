f219a83f5e47560eb0e84a7b191a12bc6ea621de
yuliantoA <yuliantoardin@gmail.com>
2020-03-16 15:24:05 +0700

    db baru

--- DB_ITENICE.sql
+++ DB_ITENICE.sql
@@ -13,26 +13,32 @@
 
 
 -- Dumping database structure for e-itenice
+DROP DATABASE IF EXISTS `e-itenice`;
 CREATE DATABASE IF NOT EXISTS `e-itenice` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `e-itenice`;
 
 -- Dumping structure for table e-itenice.tbl_account
+DROP TABLE IF EXISTS `tbl_account`;
 CREATE TABLE IF NOT EXISTS `tbl_account` (
-  `idAccount` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `NIP` varchar(11) DEFAULT NULL,
+  `Username` varchar(50) DEFAULT NULL,
   `Password` varchar(20) DEFAULT NULL,
   `Status` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`idAccount`),
+  PRIMARY KEY (`id`) USING BTREE,
   KEY `NIP` (`NIP`),
   CONSTRAINT `NIP` FOREIGN KEY (`NIP`) REFERENCES `tbl_pegawai` (`Nip`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table e-itenice.tbl_account: ~0 rows (approximately)
+-- Dumping data for table e-itenice.tbl_account: ~1 rows (approximately)
 DELETE FROM `tbl_account`;
 /*!40000 ALTER TABLE `tbl_account` DISABLE KEYS */;
+INSERT INTO `tbl_account` (`id`, `NIP`, `Username`, `Password`, `Status`) VALUES
+	(1, '1', 'ujank', '1', 'admin');
 /*!40000 ALTER TABLE `tbl_account` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_department
+DROP TABLE IF EXISTS `tbl_department`;
 CREATE TABLE IF NOT EXISTS `tbl_department` (
   `idDepartment` int(11) NOT NULL,
   `KodeDepartement` varchar(5) DEFAULT NULL,
@@ -46,6 +52,7 @@ DELETE FROM `tbl_department`;
 /*!40000 ALTER TABLE `tbl_department` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_pegawai
+DROP TABLE IF EXISTS `tbl_pegawai`;
 CREATE TABLE IF NOT EXISTS `tbl_pegawai` (
   `Nip` varchar(11) NOT NULL,
   `NamaPegawai` varchar(50) DEFAULT NULL,
@@ -58,12 +65,15 @@ CREATE TABLE IF NOT EXISTS `tbl_pegawai` (
   PRIMARY KEY (`Nip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table e-itenice.tbl_pegawai: ~0 rows (approximately)
+-- Dumping data for table e-itenice.tbl_pegawai: ~1 rows (approximately)
 DELETE FROM `tbl_pegawai`;
 /*!40000 ALTER TABLE `tbl_pegawai` DISABLE KEYS */;
+INSERT INTO `tbl_pegawai` (`Nip`, `NamaPegawai`, `TanggalLahir`, `TempatLahir`, `Alamat`, `NoHP`, `Email`, `Aktif`) VALUES
+	('1', 'ujank', '2020-03-16', 'bandung', 'kircon', '092', 'asd@asd.asd', 'Y');
 /*!40000 ALTER TABLE `tbl_pegawai` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_rapat
+DROP TABLE IF EXISTS `tbl_rapat`;
 CREATE TABLE IF NOT EXISTS `tbl_rapat` (
   `IdRapat` int(11) NOT NULL,
   `NIP` varchar(11) DEFAULT NULL,
@@ -84,21 +94,30 @@ DELETE FROM `tbl_rapat`;
 /*!40000 ALTER TABLE `tbl_rapat` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_sk
+DROP TABLE IF EXISTS `tbl_sk`;
 CREATE TABLE IF NOT EXISTS `tbl_sk` (
   `IdSK` varchar(5) NOT NULL,
   `Tema` varchar(50) DEFAULT NULL,
   `FileTemplate` varchar(50) DEFAULT NULL,
-  `Input` varchar(30) DEFAULT NULL,
-  `Jabatan` varchar(2) DEFAULT NULL,
+  `Input` longtext,
+  `Role` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`IdSK`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table e-itenice.tbl_sk: ~0 rows (approximately)
+-- Dumping data for table e-itenice.tbl_sk: ~6 rows (approximately)
 DELETE FROM `tbl_sk`;
 /*!40000 ALTER TABLE `tbl_sk` DISABLE KEYS */;
+INSERT INTO `tbl_sk` (`IdSK`, `Tema`, `FileTemplate`, `Input`, `Role`) VALUES
+	('1', 'sk admin', 'asd', '{no_surat :{\n  type:"text",\n  label:"no surat"\n},\n  tujuan :{\n    type: "array",\n    label : "kepada"\n  },\n  isi : {\n    type : "longText",\n    label: "isi surat"\n  }\n}', 'admin'),
+	('2', 'sk dosen', 'asd', '{no_surat :{\n  type:"text",\n  label:"no surat"\n},\n  tujuan :{\n    type: "array",\n    label : "kepada"\n  },\n  isi : {\n    type : "longText",\n    label: "isi surat"\n  }\n}', 'dosen'),
+	('3', 'sk dekan', 'asd', '{no_surat :{\n  type:"text",\n  label:"no surat"\n},\n  tujuan :{\n    type: "array",\n    label : "kepada"\n  },\n  isi : {\n    type : "longText",\n    label: "isi surat"\n  }\n}', 'dekan'),
+	('4', 'sk rektor', 'asd', '{no_surat :{\n  type:"text",\n  label:"no surat"\n},\n  tujuan :{\n    type: "array",\n    label : "kepada"\n  },\n  isi : {\n    type : "longText",\n    label: "isi surat"\n  }\n}', 'rektor'),
+	('5', 'sk jurusan', 'asd', '{no_surat :{', 'jurusan'),
+	('6', 'sk dosen 2', 'asd', '{no_surat :{\n  type:"text",\n  label:"no surat"\n},\n  tujuan :{\n    type: "array",\n    label : "kepada"\n  },\n  isi : {\n    type : "longText",\n    label: "isi surat"\n  }\n}', 'dosen');
 /*!40000 ALTER TABLE `tbl_sk` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_staff_departement
+DROP TABLE IF EXISTS `tbl_staff_departement`;
 CREATE TABLE IF NOT EXISTS `tbl_staff_departement` (
   `IdStaffDept` int(11) NOT NULL,
   `NIP` varchar(11) DEFAULT NULL,
@@ -117,6 +136,7 @@ DELETE FROM `tbl_staff_departement`;
 /*!40000 ALTER TABLE `tbl_staff_departement` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_staff_surat
+DROP TABLE IF EXISTS `tbl_staff_surat`;
 CREATE TABLE IF NOT EXISTS `tbl_staff_surat` (
   `IdStaffSurat` int(11) NOT NULL,
   `IdSurat` int(11) DEFAULT NULL,
@@ -135,6 +155,7 @@ DELETE FROM `tbl_staff_surat`;
 /*!40000 ALTER TABLE `tbl_staff_surat` ENABLE KEYS */;
 
 -- Dumping structure for table e-itenice.tbl_surat
+DROP TABLE IF EXISTS `tbl_surat`;
 CREATE TABLE IF NOT EXISTS `tbl_surat` (
   `IdSurat` int(11) NOT NULL,
   `IdSK` varchar(5) DEFAULT NULL,
@@ -143,6 +164,7 @@ CREATE TABLE IF NOT EXISTS `tbl_surat` (
   `TglDibuat` date DEFAULT NULL,
   `File` varchar(50) DEFAULT NULL,
   `Status` varchar(50) DEFAULT NULL,
+  `Value` longtext,
   PRIMARY KEY (`IdSurat`),
   KEY `FK_tbl_surat_tbl_sk` (`IdSK`),
   CONSTRAINT `FK_tbl_surat_tbl_sk` FOREIGN KEY (`IdSK`) REFERENCES `tbl_sk` (`IdSK`) ON DELETE CASCADE ON UPDATE CASCADE
